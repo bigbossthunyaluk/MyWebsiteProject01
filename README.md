f335017991b5605807270df646613623f0e80dc6
bigbossthunyaluk <thunyaluk2546.sasiwarinkul@gmail.com>
2022-01-07 23:44:33 +0700

    upload website

--- README.md
+++
@@ -1,1 +0,0 @@
-"# MyWebsiteProject01" 
---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.4 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzGSEfB2PH2HOiWhEnsksNMpRMte5LfUItc8EertaE4097RO1BV2KKL_LcBCaznH6d0/exec'
+const form = document.forms['google-sheet-link']
+          
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("ส่งคำอวยพรเรียบร้อยแล้ว"))
+    .catch(error => console.error('Error!', error.message))
+})
